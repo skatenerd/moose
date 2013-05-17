3d138abe2346f1a8b9159ca10b7c630d8f2fba82
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-05-17 14:10:56 -0500

    Integrate foreman and cucumber

---
+++ moose-frontend/launch_service.sh
@@ -0,0 +1,2 @@
+cd ../service
+lein run
--- service/src/moose/web/synchronous.clj
+++ service/src/moose/web/synchronous.clj
@@ -6,6 +6,8 @@
     lamina.core
     aleph.http)
   (:require
+    [moose.state :as state]
+    [moose.config :as config]
     [compojure.route :as route]))
 
 (declare page)
@@ -16,10 +18,17 @@
        :headers {"content-type" "text/html"}
        :body (page (get  (:query-params request) "name"))}))
 
+(defn attempt-reset-state [password]
+  (if (= password config/state-reset-password)
+    (state/reset-state!)
+    ))
+
 (def wrapped-sync-page-generator
   (wrap-params (wrap-aleph-handler sync-page-generator)))
 
 (defroutes sync-app-routes
+  (GET "/reset-state/:password" [id]
+    (attempt-reset-state id))
   (route/not-found wrapped-sync-page-generator))
 
 (def sync-app
